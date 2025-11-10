8b4243ca1b542c1dda888dbfb120dfb0941c24c5
24127135 <tmtri2430@clc.fitus.edu.vn>
2025-11-10 10:02:49 +0700

    Initial folder setup

---
+++ backend/trek_guide_api/urls.py
@@ -0,0 +1,14 @@
+from django.contrib import admin
+from django.urls import path
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+
+    # --- Add these two lines for JWT Authentication ---
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+]
